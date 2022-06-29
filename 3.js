bb33093eb390b43ebaa04d5253a8f4ecbdbb6f3b
le2005197 <108255906+le2005197@users.noreply.github.com>
2022-06-29 06:39:05 +0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,12 @@
+function enableEditMode(){
+    richTextField.document.designMode = "on";
+
+}
+function Edit(command){
+    richTextField.document.execCommand(command,false,null);
+}
+function execVal(command,value){
+    richTextField.document.execCommand(command,false,value);
+    
+}
+
